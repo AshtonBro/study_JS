19f44be63acaa0431cb1fb3c5338108119759133
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-08-23 22:31:29 +0500

    Обновленное сложное задание

--- study_JS/ComplicatedTask/complicatedJS.js
+++ study_JS/ComplicatedTask/complicatedJS.js
@@ -1,6 +1,25 @@
-let num = 266219;
-let numProis = 2 * 6 * 6 * 2 * 1 * 9;
-console.log('Произведение цифр:',numProis);
-console.log('Возводим в степень 3:',numProis * numProis * numProis);
+//  Создать переменную num со значением 266219 ·  Вывести в консоль произведение (умножение) цифр этого числа
 
+let num = 266219;
+let myNum = num.toString().split("");
+
+let numProis = 2 * 6 * 6 * 2 * 1 * 9;
+
+let sum = myNum[0];
+for (let i = 1; i <= myNum.length -1; i++) {
+    sum *= myNum[i];
+}
+
+//У меня не работает оператор **
+let stepen = 3;
+let result = 1;
+for (let i = 1; i <= stepen; i++){
+    result *= sum;
+}
+
+
+console.log(myNum);
+console.log('Должно быть:', numProis);
+console.log('Произведение цифр:', sum);
+console.log('Возведение степени', result);
 
