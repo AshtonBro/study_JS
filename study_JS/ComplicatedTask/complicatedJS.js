ad225ddb35c55cd953131d1e6001b0b3152335bd
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-08-25 13:40:28 +0500

    Усложнённое дз с комментариями действий

--- study_JS/ComplicatedTask/complicatedJS.js
+++ study_JS/ComplicatedTask/complicatedJS.js
@@ -1,25 +1,31 @@
 //  Создать переменную num со значением 266219 ·  Вывести в консоль произведение (умножение) цифр этого числа
 
-let num = 266219;
-let myNum = num.toString().split("");
+let num = 266219; //Обьявляем переменную num и записываем в неё число
+let numMustBe = 2 * 6 * 6 * 2 * 1 * 9; 
 
-let numProis = 2 * 6 * 6 * 2 * 1 * 9;
+let arrNum = num.toString().split(""); /*методом toString конвертирует из number в string, 
+методом .split("")  разделяет строку на каждый элемент и возвращает массив стрингов[]*/
 
-let sum = myNum[0];
-for (let i = 1; i <= myNum.length -1; i++) {
-    sum *= myNum[i];
+let arrSave = 1; //Создали переменную для сохранения результата
+for (let i = 0; i <= arrNum.length -1; i++) { /*обьявялем переменную и методом перебора проходя через ЦИКЛ меняем в ней число 
+    пока это число не будет равно длине нашего массива */
+    arrSave *= arrNum[i]; //Переменная * массив[номер индекса нашего элемента в массиве] = сохраняем результат в переменную
+  
+    console.log('Переменная для сохданения ресультат: ', arrSave);
+    console.log('Перебор елемендов по индексу: ', arrNum);
 }
 
-//У меня не работает оператор **
-let stepen = 3;
-let result = 1;
-for (let i = 1; i <= stepen; i++){
-    result *= sum;
+let Stepen = 3;
+let Result = 1;
+for (let i = 1; i <= Stepen; i++) {
+    console.log('i: ', i);
+    console.log('Result *= arrSave; : ', Result *= arrSave);
+    console.log('Result: ', Result);
 }
 
+//Вывести первые две цифры результата возведения в степень
+console.log('Вывести первые две цифры: ',Result.toString().substring(0, 2));
 
-console.log(myNum);
-console.log('Должно быть:', numProis);
-console.log('Произведение цифр:', sum);
-console.log('Возведение степени', result);
-
+console.log(arrNum);
+console.log('Производная числа: ', arrSave);
+console.log('Должно быть: ', numMustBe);
