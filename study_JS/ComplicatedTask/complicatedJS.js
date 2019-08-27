c4286bc8f9d0ab5c73c489059fb26355fefcf262
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-08-27 21:47:09 +0500

    Сделанное усложнённое задание

--- study_JS/ComplicatedTask/complicatedJS.js
+++ study_JS/ComplicatedTask/complicatedJS.js
@@ -1,46 +1,29 @@
-//  Создать переменную num со значением 266219 ·  Вывести в консоль произведение (умножение) цифр этого числа
+'use strict';
 
-let num = 26062109; //Обьявляем переменную num и записываем в неё число
-let numMustBe = 2 * 6 * 6 * 2 * 1 * 9; 
+let string30 = '      Они кричат, они грозятся:«Вот к стенке мы славян прижмем!    ';
+let argument = prompt('Введи данные а там посмотрим?', string30);
+console.log('typeof: ', typeof argument);
 
-let arrNum = num.toString().split(""); /*методом toString конвертирует из number в string, 
-методом .split("")  разделяет строку на каждый элемент и возвращает массив стрингов[]*/
+// —1 Если как аргумент передана не строка - функция оповещает об этом пользователя Check
 
- 
-console.log('Есть ли в массиве нули: ', arrNum.includes('0'));
-console.log('Массив в оригенале: ', arrNum);
+// —2 В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце срулс
 
-if (arrNum.includes('0')) {
-for (let i in arrNum) {
-    if(arrNum[i] == '0'){
-        let remuved = arrNum.splice(i, 1);
-        console.log('Индекс: ', i, " ",'Вырезан: ', remuved);
-    }
+// —3 Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них ...
+
+function filterArgument(data) {
+
+if (data.length > 0) {
+    console.log('С пробелами: ', data);
+    data = data.trim();
+    console.log('Без пробелов: ', data);
+    data = data.substring(0,30);
+    console.log('До 30 строк: ', data + '...');
+    return data;
+} else {
+    return console.log(confirm('Это не является строкой'));
 }
-console.log('Массив после фильтрации: ', arrNum);
-}
-
-let arrSave = 1; //Создали переменную для сохранения результата
-for (let i = 0; i <= arrNum.length -1; i++) { /*обьявялем переменную и методом перебора проходя через ЦИКЛ меняем в ней число 
-    пока это число не будет равно длине нашего массива */
-    arrSave *= arrNum[i]; //Переменная * массив[номер индекса нашего элемента в массиве] = сохраняем результат в переменную
-  
-    console.log('Переменная для сохданения ресультат: ', arrSave);
-    console.log('Перебор елемендов по индексу: ', arrNum);
 }
 
-let Stepen = 3;
-let Result = 1;
-for (let i = 1; i <= Stepen; i++) {
-    console.log('i: ', i);
-    console.log('Result *= arrSave; : ', Result *= arrSave);
-    console.log('Result: ', Result);
-}
 
-//Вывести первые две цифры результата возведения в степень
-console.log('Вывести первые две цифры: ',Result.toString().substring(0, 2));
+filterArgument(argument);
 
-//Вывод общего результат
-console.log(arrNum);
-console.log('Производная числа: ', arrSave);
-console.log('Должно быть: ', numMustBe);
