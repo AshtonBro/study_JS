92b0e5b4227085e68a3eb27da8f12fad41125464
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-09-03 03:26:16 +0500

    Добавитель тега li

--- study_JS/ForPractice/practiceSurTask.js
+++ study_JS/ForPractice/practiceSurTask.js
@@ -1,34 +1,19 @@
+/*
+Добавлять новые <li> элементы
+с текстом из инпута
 
-let arr = [15,1,2,3,0,12,4];
-console.log('Original: ', arr);
-let ArrResult = [];
+*/
 
-funcNum(arr);
+function newLiElements() {
 
-function funcNum(massiv) {
-
-    console.log('length: ', massiv.length);
-
-    for (let i = 0; i < massiv.length; i++){
-        
-        console.log('massiv[i]: ', massiv[i]);
-        
-        let lessCount = 0;
-
-            for(let j = i; j < massiv.length; j++) 
-            {
-                if (massiv[i] > massiv[j]) {
-                    lessCount += 1;
-                }
-                
-            } 
-        
-        ArrResult.push(lessCount);
-        // console.log('lessCount: ', lessCount);
-
-    }
-
-  console.log('ArrResult: ', ArrResult);
-} 
-
-
+    let imputText = document.getElementById('imput').value;
+    let list = document.querySelector('#listOfLi');
+    
+    let liCollect = document.createElement('Li');
+    let txtNode = document.createTextNode(imputText);
+    
+    liCollect.appendChild(txtNode);
+    list.appendChild(liCollect);
+    
+    console.log('list: ', list);
+}
