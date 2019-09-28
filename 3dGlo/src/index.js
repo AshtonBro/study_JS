544799ce94ccf353d8ba189a699f8ef76365461d
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-09-28 13:50:45 +0500

    renamed

--- 3dGlo/src/index.js
+++ 3dGlo/src/index.js
@@ -1,39 +1,31 @@
 'use strict';
-import countTimer from '.modules/countTimer';
-import toggleMenu from '.modules/toggleMenu';
-import togglePopup from '.modules/togglePopup';
-import tabs from '.modules/tabs';
-import slider from '.modules/slider';
-import changePicture from '.modules/changePicture';
-import validation from '.modules/validation';
-import calculator from '.modules/calculator';
-import sendForm from '.modules/sendForm';
 
+import countTimer from './modules/countTimer';
+import toggleMenu from './modules/toggleMenu';
+import togglePopup from './modules/togglePopup';
+import tabs from './modules/tabs';
+import slider from './modules/slider';
+import changePicture from './modules/changePicture';
+import validation from './modules/validation';
+import calculator from './modules/calculator';
+import sendForm from './modules/sendForm';
 
 // Timer
 countTimer();
-
 // Menu
 toggleMenu();
-
 // popup окно
 togglePopup();
-
 // Табы 
 tabs();
-
 // Слайдер 
 slider();
-
 // Наша каманда, работа с img и dataset
 changePicture();
-
 // regex калькулятора валидация для цифр
 validation();
-
 // калькулятор типа обьекта
 calculator();
-
 //send-ajax-form
 sendForm();
 
