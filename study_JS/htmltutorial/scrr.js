acfe47b78a18547b69cc42f24dcfe8b832b5e5e9
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-09-29 18:33:14 +0500

    удаление лишнего

--- study_JS/htmltutorial/scrr.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let collect = document.querySelectorAll('.collections');
-let elem = document.querySelectorAll('.elem');
-
-
-let newElem = document.createElement('li');
-newElem.textContent = 'Изучай ВСЁ';
-collect[1].appendChild(newElem);
