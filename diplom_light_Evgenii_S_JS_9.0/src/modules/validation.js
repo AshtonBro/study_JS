46a1102ec6e4c99c088568e820d3678c83e3ceed
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-05 22:30:22 +0500

    New modul validation

---
+++ diplom_light_Evgenii_S_JS_9.0/src/modules/validation.js
@@ -0,0 +1,12 @@
+const validation = () => {
+    document.addEventListener('input', (event) => {
+        let element = event.target;
+        if(element.classList.contains('phone-user')) {
+            element.value = element.value.replace(/[^0-9]/g, '');
+        } else if (element.classList.contains('user-name')) {
+            element.value = element.value.replace(/[^а-яА-ЯёЁ\ ]/g, '');
+        }
+    });
+};
+
+export default validation;
