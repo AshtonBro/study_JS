87262f30a90b8e1906b73142de112396ce592900
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-05 22:29:58 +0500

    add modul validation

--- diplom_light_Evgenii_S_JS_9.0/src/index.js
+++ diplom_light_Evgenii_S_JS_9.0/src/index.js
@@ -2,9 +2,11 @@
 
 import popUpWindowOne from './modules/popUpWindowOne';
 import sendForm from './modules/sendForm';
-
+import validation from './modules/validation';
 
 // Модальное окно номер 1
 popUpWindowOne();
 // Отправка формы для блока 1 и блока 2
 sendForm();
+// Валидация форм
+validation();
