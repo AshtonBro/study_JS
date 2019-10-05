d1b639169eade79209dfb1b8be90f2c25e4e7a8f
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-05 22:37:18 +0500

    Добавил валидацию (можно "+")

--- diplom_light_Evgenii_S_JS_9.0/src/modules/validation.js
+++ diplom_light_Evgenii_S_JS_9.0/src/modules/validation.js
@@ -2,7 +2,7 @@ const validation = () => {
     document.addEventListener('input', (event) => {
         let element = event.target;
         if(element.classList.contains('phone-user')) {
-            element.value = element.value.replace(/[^0-9]/g, '');
+            element.value = element.value.replace(/[^0-9+]/g, '');
         } else if (element.classList.contains('user-name')) {
             element.value = element.value.replace(/[^а-яА-ЯёЁ\ ]/g, '');
         }
