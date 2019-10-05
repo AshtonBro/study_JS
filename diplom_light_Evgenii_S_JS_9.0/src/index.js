862adde719e2b9045955d78d47569a93669452b2
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-05 19:46:33 +0500

    add sendForm modul

--- diplom_light_Evgenii_S_JS_9.0/src/index.js
+++ diplom_light_Evgenii_S_JS_9.0/src/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 import popUpWindowOne from './modules/popUpWindowOne';
-
+import sendForm from './modules/sendForm';
 
 
 // Модальное окно номер 1
 popUpWindowOne();
+// Отправка формы для блока 1 и блока 2
+sendForm();
