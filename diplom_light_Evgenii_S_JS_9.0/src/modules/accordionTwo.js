b73758fc196f30da4856a1956584b547ea777de7
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-06 17:08:21 +0500

    AccordionTwo

---
+++ diplom_light_Evgenii_S_JS_9.0/src/modules/accordionTwo.js
@@ -0,0 +1,30 @@
+const accordionTwo = () => {
+    const questions = document.querySelector('.questions'),
+    panelHeading = questions.querySelectorAll('.panel-heading'),
+    collapse = questions.querySelectorAll('.collapse'); 
+
+    const toggleCollapse = (index) => {
+        for(let i = 0; i < panelHeading.length; i++){
+            if(index === i) {
+                 collapse[i].classList.add('in');
+            } else {
+                 collapse[i].classList.remove('in');
+            }
+        }
+    };
+    
+    questions.addEventListener('click', (event) => {
+        let target = event.target;
+        target = target.closest('.panel-heading');
+        if(target) {
+            panelHeading.forEach((item, i) => {
+                if(item === target){
+                    toggleCollapse(i);
+                }
+            });
+        }
+    });
+};
+
+export default accordionTwo;
+
