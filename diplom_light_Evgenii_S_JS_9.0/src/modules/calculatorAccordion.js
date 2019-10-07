ccb4203cab51329c84a3719392490793ff061369
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-07 23:27:36 +0500

    Calculator

--- diplom_light_Evgenii_S_JS_9.0/src/modules/calculatorAccordion.js
+++ diplom_light_Evgenii_S_JS_9.0/src/modules/calculatorAccordion.js
@@ -1,12 +1,10 @@
 class CalculatorAccordion {
   constructor (){
-    this.theWell = document.getElementById('myonoffswitch');
-    this.theBottomCheck = document.getElementById('myonoffswitch-two');
-    this.secondWill = document.querySelectorAll('.second-will');
-    this.accordion = document.getElementById('accordion');
-    this.expandAll = document.querySelectorAll('.expand');
+    this.chbIsSingle = document.getElementById('myonoffswitch');
+    this.chbIsBottom = document.getElementById('myonoffswitch-two');
+    this.isHidden = document.querySelectorAll('.second-will');
     this.totalResult = document.getElementById('calc-result');
-    this.selectAll = [...document.querySelectorAll('select')];
+    this.selectors = [...document.querySelectorAll('select')];
     this.inputs = document.querySelectorAll('input');
     
     this.body = {
@@ -17,37 +15,37 @@ class CalculatorAccordion {
   }
 
   calcSection1() {
-    if (this.theWell.checked) {
+    if (this.chbIsSingle.checked) {
       this.body.total = +10000;
-      this.secondWill.forEach( item => item.classList.add('hidden'));
+      this.isHidden.forEach( item => item.classList.add('hidden'));
       this.body.whichSeptic.splice(0,1,'Однокамерный');
-    } else if (!this.theWell.checked) {
+    } else if (!this.chbIsSingle.checked) {
       this.body.total = +15000;
-      this.secondWill.forEach( item => item.classList.remove('hidden'));
+      this.isHidden.forEach( item => item.classList.remove('hidden'));
       this.body.whichSeptic.splice(0,1,'Двухкамерный');
     } 
   }
 
   calcSection2() {
-    let isTwo = !this.theWell.checked;
+    let isTwo = !this.chbIsSingle.checked;
     let diametrProcent = 0;
-    if (this.selectAll[0].value == 1 ){
+    if (this.selectors[0].value == 1 ){
       diametrProcent += 0.2;
     }
-    if(isTwo && this.selectAll[2].value == 1) {
+    if(isTwo && this.selectors[2].value == 1) {
       diametrProcent += 0.2;
     }
     this.body.total += this.body.total * diametrProcent;
     let ringsProcent = 0;
-    if (this.selectAll[1].value == 1){
+    if (this.selectors[1].value == 1){
       ringsProcent += 0.3;
-    } else if (this.selectAll[1].value == 2){
+    } else if (this.selectors[1].value == 2){
       ringsProcent += 0.5;
     }
     if(isTwo) {
-      if (this.selectAll[3].value == 1){
+      if (this.selectors[3].value == 1){
         ringsProcent += 0.3;
-      } else if (this.selectAll[3].value == 2){
+      } else if (this.selectors[3].value == 2){
         ringsProcent += 0.5;
       }
     }
@@ -55,8 +53,8 @@ class CalculatorAccordion {
   }
 
   calcSection3() {
-    let isTwo = !this.theWell.checked;
-    let isFilterSelected = this.theBottomCheck.checked;
+    let isTwo = !this.chbIsSingle.checked;
+    let isFilterSelected = this.chbIsBottom.checked;
     if(isFilterSelected) {
       this.body.total += 1000 * (isTwo + 1);
     }
@@ -75,14 +73,14 @@ class CalculatorAccordion {
   }
 
   init() {
-    this.theWell.checked = false; 
-    this.theBottomCheck.checked = false;
+    this.chbIsSingle.checked = false; 
+    this.chbIsBottom.checked = false;
     
-    this.theWell.addEventListener('change', this.calculateAll.bind(this));
-    for (let i = 0; i < this.selectAll.length; i++) {
-      this.selectAll[i].addEventListener('change', this.calculateAll.bind(this));
+    this.chbIsSingle.addEventListener('change', this.calculateAll.bind(this));
+    for (let i = 0; i < this.selectors.length; i++) {
+      this.selectors[i].addEventListener('change', this.calculateAll.bind(this));
     }
-    this.theBottomCheck.addEventListener('change', this.calculateAll.bind(this));
+    this.chbIsBottom.addEventListener('change', this.calculateAll.bind(this));
     this.inputs[5].addEventListener('input', this.saveSection4.bind(this));
   }
 
