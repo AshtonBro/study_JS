4e976b57428127db1c462f0a2e1204b0aed1edf7
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-09 20:53:59 +0500

    очистка ипутов

--- diplom_light_Evgenii_S_JS_9.0/src/modules/modalWindows.js
+++ diplom_light_Evgenii_S_JS_9.0/src/modules/modalWindows.js
@@ -1,7 +1,8 @@
 const popUpWindowOne = () => {
     const togglePopup = (elem, btn) => {
         const popup = document.querySelector(elem);
-        document.body.addEventListener('click', (event) => {
+        const inputs = document.querySelectorAll('.user');
+        document.addEventListener('click', (event) => {
           let target = event.target;
           if (target.closest(btn)) {
             popup.style.display = 'block';
@@ -14,17 +15,19 @@ const popUpWindowOne = () => {
             popup.style.transform = 'translateX(-100%)';
             popup.style.transition = '0.8s';
             window.statusMsg.remove('div');
+            inputs.forEach(item => item.value = '');
           } else {
             target = target.closest('.popup-content');
             if (!target) {
                 popup.style.transform = 'translateX(-100%)';
                 popup.style.transition = '0.8s';
                 window.statusMsg.remove('div');
+                inputs.forEach(item => item.value = '');
             }
           }
         });
     };
-    
+
     togglePopup('.popup-call', '.call-btn');
     togglePopup('.popup-discount', '.calc-btn');
     togglePopup('.popup-check', '.check-btn');
